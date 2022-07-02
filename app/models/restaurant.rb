3ecb3db2fb7f5456a9791f73535988c3ff716c3f
gabriel roux <rouxgab81@gmail.com>
2022-07-02 12:25:40 -0400

    phone for restaurant done + improve validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,6 @@ class Restaurant < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
-  validates :address, presence: true
+  validates :address, presence: true, length: { minimum: 5 }
+  validates :phone, presence: true, format: { with: /\A\d{1}-\d{3}-\d{3}-\d{4}\z/ }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_01_235919) do
+ActiveRecord::Schema.define(version: 2022_07_02_155941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_07_01_235919) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "phone"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
