6131479b223e51a0128138d941fa0c7a8c321919
gabriel roux <rouxgab81@gmail.com>
2022-11-15 14:19:00 -0500

    ingredient view done + modify meal view + gem openfood install

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,4 +1,5 @@
 class IngredientsController < ApplicationController
+    before_action :set_meal, only: [:new, :create, :edit, :update]
 
     def index
         @ingredients = Ingredient.all
@@ -23,6 +24,7 @@ class IngredientsController < ApplicationController
         # raise
         # @ingredient.info = param
         @product = Openfoodfacts::Product.get(@ingredient.code, locale: 'fr')
+        @ingredient.meal = @meal
         @ingredient.name = @product.product_name
         @ingredient.info = @product.nutriments.to_hash
         @ingredient.image = @product["image_front_small_url"]
@@ -30,10 +32,20 @@ class IngredientsController < ApplicationController
         redirect_to ingredient_path(@ingredient)
     end
 
+    def destroy
+        @ingredient = Ingredient.find(params[:id])
+        @ingredient.destroy
+        redirect_to meal_path(@ingredient.meal_id)
+    end
+
     private
+
+    def set_meal
+        @meal = Meal.find(params[:meal_id])
+    end
 
     def ingredient_params
         params.require(:ingredient).permit(:code)
     end
-    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :restaurants do
     resources :meals, only: [ :new, :create, :edit, :update] do
-      resources :ingredients
+      resources :ingredients, only: [ :new, :create, :edit, :update]
     end
   end
 
-  resources :meals, only: :destroy
+  resources :meals, only: [:show, :destroy]
+  resources :ingredients, only: [:show, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
