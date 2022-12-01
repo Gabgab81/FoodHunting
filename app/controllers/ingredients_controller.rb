05634592522b38f156c5d06c2da2fba49107c4a6
gabriel roux <rouxgab81@gmail.com>
2022-12-01 10:04:57 -0500

    search put in the navbar

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -29,9 +29,10 @@ class IngredientsController < ApplicationController
         @ingredient.info = @product.nutriments.to_hash
         @ingredient.image = @product["image_front_small_url"]
         authorize @ingredient
+        # raise
         if @ingredient.save
             if @meal.ingredients.count > 0
-                @meal.protein = @meal.ingredients.inject(0) {|sum, ingredient| sum + (ingredient.info["proteins_100g"] * ingredient.weight)/100}
+                @meal.protein = @meal.ingredients.inject(0) {|sum, ingredient| sum + (ingredient.info["proteins_100g"]* ingredient.weight)/100}
                 @meal.carbohydrate = @meal.ingredients.inject(0) {|sum, ingredient| sum + (ingredient.info["carbohydrates_100g"] * ingredient.weight)/100}
                 @meal.fat = @meal.ingredients.inject(0) {|sum, ingredient| sum + (ingredient.info["fat_100g"] * ingredient.weight)/100}
             else
