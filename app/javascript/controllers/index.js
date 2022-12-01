bb84ecf9ce1f027a08a1cc815e319e9820dfd96f
gabriel roux <rouxgab81@gmail.com>
2022-12-01 18:29:39 -0500

    stimulus for card restaurant done

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,6 +7,9 @@ import { application } from "./application"
 import AddressAutocompleteController from "./address_autocomplete_controller"
 application.register("address-autocomplete", AddressAutocompleteController)
 
+import DefilementImgController from "./defilement_img_controller"
+application.register("defilement-img", DefilementImgController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
