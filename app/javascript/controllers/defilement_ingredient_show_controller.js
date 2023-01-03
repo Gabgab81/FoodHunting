6e9f21e2e1846e35bbfd55b28d8f80f5b5b50d22
gabriel roux <rouxgab81@gmail.com>
2023-01-03 12:20:52 -0500

    fix javascript for list ingredient

--- app/javascript/controllers/defilement_ingredient_show_controller.js
+++ app/javascript/controllers/defilement_ingredient_show_controller.js
@@ -8,23 +8,34 @@ export default class extends Controller {
   }
   connect() {
     this.height = this.element.querySelector('.info-meal').offsetHeight
+
     this.position = 0
+    
     this.allCardNode = this.element.querySelectorAll('.list-ingredients > .card-ingredient');
     this.totalHeightCards = 0
     this.allCardNode.forEach((node) => this.totalHeightCards += node.offsetHeight)
-
+    this.element.querySelector('.btn-top').style.display = 'none'
+   
     if (this.totalHeightCards + 70 < this.height) {
       this.element.querySelector('.btn-top').remove()
       this.element.querySelector('.btn-bottom').remove()
       this.element.querySelector('.list-ingredients').style.height = `${this.height}px`
     } else {
-      this.element.querySelector('.list-ingredients').style.height = `${this.height - 200}px`
+      this.element.querySelector('.list-ingredients').style.height = `${this.height - 90}px`
     }
   }
 
   up(){
-    this.position > 200 ? this.position -= 200 : this.position = 0
-    console.log('position:', this.position )
+
+    if ((this.position > this.firstCardHeight * 2) && (this.totalHeightCards > this.listIngredient + this.lastCardHeight)) {
+      this.position -= 200
+      this.element.querySelector('.btn-bottom').style.display = 'block'
+    } else {
+      this.position = 0
+      this.element.querySelector('.btn-top').style.display = 'none'
+      this.element.querySelector('.btn-bottom').style.display = 'block'
+      
+    }
     this.cardsTargets[0].scrollTo({
       top: this.position,
       left: 0,
@@ -33,12 +44,33 @@ export default class extends Controller {
   }
 
   down(){
-    this.position < this.height - 200 ? this.position += 200 : this.position = this.height
-    console.log('position:', this.position )
-    this.cardsTargets[0].scrollTo({
-      top: this.position,
-      left: 0,
-      behavior: 'smooth'
-    });
+
+    this.allCardNode = this.element.querySelectorAll('.list-ingredients > .card-ingredient');
+    this.firstCardHeight = this.allCardNode[0].offsetHeight;
+    this.lastCardHeight = this.allCardNode[this.allCardNode.length - 1].offsetHeight;
+   
+    this.totalHeightCards = 0
+    this.allCardNode.forEach((node) => this.totalHeightCards += node.offsetHeight)
+
+    this.listIngredient = this.element.querySelector('.list-ingredients').offsetHeight
+
+    if ((this.position < this.totalHeightCards - this.lastCardHeight * 4) && (this.totalHeightCards > this.listIngredient + this.lastCardHeight)) {
+      this.position += 200
+      this.element.querySelector('.btn-top').style.display = 'block'
+      this.cardsTargets[0].scrollTo({
+        top: this.position,
+        left: 0,
+        behavior: 'smooth'
+      });
+    } else {
+      this.cardsTargets[0].scrollTo({
+        top: this.totalHeightCards,
+        left: 0,
+        behavior: 'smooth'
+      });
+      this.position = this.totalHeightCards - this.listIngredient
+      this.element.querySelector('.btn-bottom').style.display = 'none'
+      this.element.querySelector('.btn-top').style.display = 'block'
+    }
   }
 }
