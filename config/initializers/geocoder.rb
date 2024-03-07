7d89f08667bbb0d942f6aa6744e2c16fe21792a4
gabriel roux <rouxgab81@gmail.com>
2024-03-06 19:36:19 -0500

    fix the search

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,8 +1,8 @@
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
-  # lookup: :nominatim,         # name of geocoding service (symbol)
-  lookup: :mapbox,
+  lookup: :nominatim,         # name of geocoding service (symbol)
+  # lookup: :mapbox,
   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
